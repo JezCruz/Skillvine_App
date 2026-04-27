e448a13f5fb20eb4cf07c2e01dbb65cf3993fc06
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-28 05:53:26 +0800

    Skillvine – update title changed 'App Updats'

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -217,7 +217,7 @@ export default function AppLayout() {
         <Drawer.Screen name="lesson/[id]" options={{ title: 'Lesson Details', drawerItemStyle: { display: 'none' } }} />
         <Drawer.Screen name="lesson/[id]/edit" options={{ title: 'Edit Lesson', drawerItemStyle: { display: 'none' } }} />
         <Drawer.Screen name="edit-profile" options={{ title: 'Edit Profile', drawerItemStyle: { display: 'none' } }} />
-        <Drawer.Screen name="updates" options={{ title: 'Check for latest Updates' }} />
+        <Drawer.Screen name="updates" options={{ title: 'App Updates' }} />
       </Drawer>
 
       <StatusBar style="light" />
